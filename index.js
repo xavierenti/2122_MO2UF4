d3813209bc367ccaca5adab23ee0efdcf00d2f0d
enti <enti@enti.enti>
2022-02-04 11:20:36 +0100

    Pequeño servidor web

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 #!/usr/bin/node
 
-
-
-
 let http = require("http");
+
+let mongo_client = require("mongodb").MongoClient;
+
+let url = "mongodb://localhost/";
+
+console.log("Iniciando script mongo-http");
+
+mongo_client.connect(url, function(error, conn){
+	console.log("Dentro de mongoDB");
+
+	if(error){
+		console.log("ERROR!!!");
+		return;
+	}
+
+	let db = conn.db("tffhd");
+
+	let characters = db.collection("characters").find();
+
+	console.log(characters);
+
+});
+
+
+/*
 
 http.createServer(function(req, res){
 	res.writeHead(200);
@@ -14,3 +36,5 @@ http.createServer(function(req, res){
 	res.end(saludo);
 
 }).listen(1095);
+
+*/
